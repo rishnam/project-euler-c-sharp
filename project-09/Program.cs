07bcd15f15a7ae2bec50f761047233b54caebbaa
rishnam <rishi.namboo@gmail.com>
2019-08-03 23:36:17 +0100

    Project-09

---
+++ project-09/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            pythagoreanTriplets(1000);
+            Console.ReadLine();
+        }
+        static void pythagoreanTriplets(int limit)
+        {
+ 
+            int a, b, c = 0;
+            int m = 2;
+            while (c < limit)
+            {
+                for (int n = 1; n < m; ++n)
+                {
+                    a = m * m - n * n;
+                    b = 2 * m * n;
+                    c = m * m + n * n;
+
+                    if (c > limit)
+                        break;
+
+
+                    if (a+b+c == 1000)
+                    {
+                        Console.WriteLine("{0},{1},{2}", a, b, c);
+
+                        Console.WriteLine(a * b * c);
+                    }
+                    
+                }
+                m++;
+            }
+        }
+    }
+}
